6fa1453300e286330d78d0c8ec047b5cd7e66315
IrinaGC <irina.g.cirlan@gmail.com>
2021-07-05 17:07:34 +0100

    TDD US-2,3,4

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,16 +1,41 @@
 "use strict";
 
-// Thermostat starts at 20 degrees
 
 describe ("Thermostat", () => {
   let thermostat;
-
+  
   beforeEach(() => {
     thermostat = new Thermostat();
   });
-
+  
+  // Thermostat starts at 20 degrees
   it("starts at 20 degrees", () => {
-    expect(thermostat.startTemperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 
-});
+  // You can increase the temperature with an up function
+  it("increase temperature with 1", () => {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  // You can decrease the temperature with a down function
+  it("decrease temperature with 1", () => {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  // The minimum temperature is 10 degrees
+  it('error below 10 degrees', () => {
+    for (let i=0; i<10; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+
+})
+
+// expect(function() {
+//         player.resume();
+//       }).toThrowError("song is already playing");
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,6 +1,29 @@
 
 class Thermostat {
-  constructor () {
-    this.startTemperature = 20;
+  
+  constructor() {
+    this.MIN_TEMP = 10;
+    this.temp = 20;
   };
-};
+
+  getCurrentTemperature() {
+    return this.temp;
+  }
+
+  isMinTemp() {
+    return this.temp === this.MIN_TEMP;
+  }
+
+  up() {
+     this.temp += 1;
+   }
+
+   down() {
+     if (this.MinTemp) {
+       return;
+      } 
+      this.temp -= 1;
+    };
+
+    
+    }
